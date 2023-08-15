750bf28979dcc0fb1c09f36fe2d3b3c631502ee8
cobategit <mancunianmerchant816@gmail.com>
2023-08-15 14:53:14 +0700

    insertinto

--- src/handlers/present/procedureInsertReportPresent.ts
+++ src/handlers/present/procedureInsertReportPresent.ts
@@ -2,11 +2,19 @@ import { LoggersApp } from '@jpj-common/module'
 import { PresentServices } from '../../services'
 import dotenv from 'dotenv'
 import path from 'path'
+import { jumpDays } from '../../utils'
+import { format, isWeekend } from 'date-fns'
 
 export const procedureInsertIntoReport = async () => {
     dotenv.config({ path: path.join(__dirname, './../../../.env') })
     LoggersApp.configureLogger()
-    const res = await PresentServices.procedureInsertIntoReport()
+    const beforeOneDate = jumpDays("kurang", 1)
+    const formatBeforeOneDate = `${format(beforeOneDate!, 'yyyy-MM-dd')}`
+    let res: Record<string, any> = {}
+
+    if (!isWeekend(new Date(`${formatBeforeOneDate}`))) {
+        res = await PresentServices.procedureInsertIntoReport()
+    }
     LoggersApp.info("return call procedure insert into report", res)
 }
 
